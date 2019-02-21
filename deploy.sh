d7a8ec62dc9c93014510ae9a33037039e0be1fda
Julien Pivotto <roidelapluie@inuits.eu>
2019-02-21 11:28:41 +0100

    Fix mkdir
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ git commit -m 'Automated Build' && \
 git push -f git@github.com:roidelapluie/roidelapluie.be-output.git master
 
 rm -rf .git && \
-find . -type d -print0 | xargs -0 -L 1 -P 10 ../deploy-dir.sh
+find . -type d -print0 | xargs -0 -L 1 -P 10 ../deploy-dir.sh || true
 find . -type f -print0 | xargs -0 -L 1 -P 10 ../deploy-file.sh
 
